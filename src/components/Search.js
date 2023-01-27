ec6fab7ebac36f13c93ff65074efb69ca270d1dd
Jonathanbir <jonathan79111@gmail.com>
2023-01-27 19:52:15 +0800

    commit: 新增 Search 功能

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 
-const Search = ({ search, setInput }) => {
-  const inputHandler = (e) => {
-    setInput(e.target.value);
+const Search = () => {
+  const auth = "D5N3ySbIbgJRhQyIzul5r7yuza62xggjx72HxqsoyuVc5CHOkP6P2ciz";
+  const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+  const search = async () => {
+    let result = await axios.get(initialURL, {
+      headers: { Authorization: auth },
+    });
+    console.log(result);
   };
 
   return (
     <div className="search">
-      <input className="input" onChange={inputHandler} type="text" />
+      <input className="input" onChange={() => {}} type="text" />
       <button onClick={search}>Search</button>
     </div>
   );
--- src/pages/Hoempage.js
+++ src/pages/Hoempage.js
@@ -2,7 +2,6 @@ import React from "react";
 import Search from "../components/Search";
 
 const Hoempage = () => {
-  const auth = "D5N3ySbIbgJRhQyIzul5r7yuza62xggjx72HxqsoyuVc5CHOkP6P2ciz";
   return (
     <div style={{ minHeight: "100vh" }}>
       <Search />
