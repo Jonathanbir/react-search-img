60eaffeb7eb5b29a8217291cf1e51b8c2545cdbe
Jonathanbir <jonathan79111@gmail.com>
2023-01-27 20:22:08 +0800

    commit: 完成剩餘功能 搜尋 與 更新

--- src/pages/Hoempage.js
+++ src/pages/Hoempage.js
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
 import Search from "../components/Search";
 import Picture from "../components/Picture";
 
-const Hoempage = () => {
-  let [input, setInput] = useState(null);
+const Homepage = () => {
+  let [input, setInput] = useState("");
   let [data, setData] = useState(null);
   let [page, setPage] = useState(1);
   let [currentSearch, setCurrentSearch] = useState("");
   const auth = "D5N3ySbIbgJRhQyIzul5r7yuza62xggjx72HxqsoyuVc5CHOkP6P2ciz";
   const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
-  const searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
+  let searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
 
   const search = async (url) => {
     let result = await axios.get(url, {
@@ -46,7 +45,12 @@ const Hoempage = () => {
 
   return (
     <div style={{ minHeight: "100vh" }}>
-      <Search search={() => search(searchURL)} setInput={setInput} />
+      <Search
+        search={() => {
+          search(searchURL);
+        }}
+        setInput={setInput}
+      />
       <div className="pictures">
         {/* logical operator && */}
         {data &&
@@ -61,4 +65,4 @@ const Hoempage = () => {
   );
 };
 
-export default Hoempage;
+export default Homepage;
