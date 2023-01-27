4a728f28660d2a22ab0989769cd48843f1e96323
Jonathanbir <jonathan79111@gmail.com>
2023-01-27 20:16:16 +0800

    commit: 更新頁面part1

--- src/pages/Hoempage.js
+++ src/pages/Hoempage.js
@@ -1,21 +1,42 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
 import Search from "../components/Search";
 import Picture from "../components/Picture";
 
-const Hoempage = () => {
-  let [input, setInput] = useState(null);
+const Homepage = () => {
+  let [input, setInput] = useState("");
   let [data, setData] = useState(null);
+  let [page, setPage] = useState(1);
+  let [currentSearch, setCurrentSearch] = useState("");
   const auth = "D5N3ySbIbgJRhQyIzul5r7yuza62xggjx72HxqsoyuVc5CHOkP6P2ciz";
   const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
-  const searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
+  let searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
 
   const search = async (url) => {
     let result = await axios.get(url, {
       headers: { Authorization: auth },
     });
     setData(result.data.photos);
+    setCurrentSearch(input);
+  };
+
+  // Closure
+  const morePicture = async () => {
+    let newURL;
+    setPage(page + 1);
+    if (currentSearch === "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page + 1}&per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${
+        page + 1
+      }`;
+    }
+    console.log("正在morePicture內部");
+    console.log(newURL);
+    let result = await axios.get(newURL, {
+      headers: { Authorization: auth },
+    });
+    setData(data.concat(result.data.photos));
   };
 
   useEffect(() => {
@@ -24,16 +45,23 @@ const Hoempage = () => {
 
   return (
     <div style={{ minHeight: "100vh" }}>
-      <Search search={() => search(searchURL)} setInput={setInput} />
+      <Search
+        search={() => {
+          search(searchURL);
+        }}
+        setInput={setInput}
+      />
       <div className="pictures">
-        {/* logical operator && */}
         {data &&
           data.map((d) => {
             return <Picture data={d} />;
           })}
       </div>
+      <div className="morePicture">
+        <button onClick={morePicture}>更多圖片</button>
+      </div>
     </div>
   );
 };
 
-export default Hoempage;
+export default Homepage;
